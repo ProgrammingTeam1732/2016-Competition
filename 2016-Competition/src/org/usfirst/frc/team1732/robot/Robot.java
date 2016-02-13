6c96ceb310f882d57f35d5badb97f91d3063d239
Blake Ziolkowski <zylviij@gmail.com>
2016-02-13 14:54:55 -0600

    dude like totally, woah, such descriptive

--- 2016-Competition/src/org/usfirst/frc/team1732/robot/Robot.java
+++ 2016-Competition/src/org/usfirst/frc/team1732/robot/Robot.java
@@ -1,96 +1,146 @@
-
 package org.usfirst.frc.team1732.robot;
 
-
 import edu.wpi.first.wpilibj.SampleRobot;
-import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * This is a demo program showing the use of the RobotDrive class.
- * The SampleRobot class is the base of a robot application that will automatically call your
- * Autonomous and OperatorControl methods at the right time as controlled by the switches on
- * the driver station or the field controls.
- *
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the SampleRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- *
- * WARNING: While it may look like a good choice to use for your code if you're inexperienced,
- * don't. Unless you know what you are doing, complex code will be much more difficult under
- * this system. Use IterativeRobot or Command-Based instead if you're new.
- */
 public class Robot extends SampleRobot {
-    RobotDrive myRobot;
-    Joystick stick;
-    final String defaultAuto = "Default";
-    final String customAuto = "My Auto";
-    SendableChooser chooser;
 
-    public Robot() {
-        myRobot = new RobotDrive(0, 1);
-        myRobot.setExpiration(0.1);
-        stick = new Joystick(0);
-    }
-    
-    public void robotInit() {
-        chooser = new SendableChooser();
-        chooser.addDefault("Default Auto", defaultAuto);
-        chooser.addObject("My Auto", customAuto);
-        SmartDashboard.putData("Auto modes", chooser);
-    }
+	final int STICK_LEFT_VERT = 1;
+	final int STICK_LEFT_HORI = 2;
+	final int STICK_RIGHT_VERT = 3;
+	final int STICK_TIGHT_HORT = 4;
+	
+	final double MOTOR_MIN = 0.1;
+	
+	final double ARM_POS_TOP = 45; 	// degrees
+	final double ARM_POS_BOT =  0; 	// degrees
+	
+	final int BUTTON_A = 0;
+	final int BUTTON_B = 1;
+	final int BUTTON_X = 2;
+	final int BUTTON_Y = 3;
+	final int BUTTON_R = 4;
+	final int BUTTON_L = 5;
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
-	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
-	 * Dashboard, remove all of the chooser code and uncomment the getString line to get the auto name from the text box
-	 * below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the if-else structure below with additional strings.
-	 * If using the SendableChooser make sure to add them to the chooser code above as well.
+
+	CANTalon left_1;
+	CANTalon left_2;
+	CANTalon left_3;
+	Encoder left_encoder;
+
+	CANTalon right_1;
+	CANTalon right_2;
+	CANTalon right_3;
+	Encoder right_encoder;
+
+	CANTalon intakarino;
+	Solenoid intakamatic;
+
+	Solenoid magic_fingers;
+	CANTalon cocker;
+	Solenoid firer;
+
+	CANTalon extendo;
+	Encoder extendo_encoder;
+	DigitalInput extendo_top;
+	DigitalInput extendo_bot;
+
+	CANTalon that_thing_that_hangs_1;
+	CANTalon that_thing_that_hangs_2;
+	Encoder that_thing_that_hangs_encoder;
+
+	CANTalon defensomatic;
+	
+	Joystick controller;
+
+	public Robot() {
+		left_1 = new CANTalon(0);
+		left_2 = new CANTalon(1);
+		left_3 = new CANTalon(2);
+		right_1 = new CANTalon(3);
+		right_2 = new CANTalon(4);
+		right_3 = new CANTalon(6);
+
+		intakarino = new CANTalon(7);
+
+		magic_fingers = new Solenoid(20, 0);
+		cocker = new CANTalon(8);
+		firer = new Solenoid(20, 1);
+
+		extendo = new CANTalon(9);
+		extendo_encoder = new Encoder(0, 1);
+		extendo_top = new DigitalInput(2);
+		extendo_bot = new DigitalInput(3);
+		
+		that_thing_that_hangs_1 = new CANTalon(10);
+		that_thing_that_hangs_2 = new CANTalon(10);
+
+		defensomatic = new CANTalon(11);
+
+		controller = new Joystick(0);
+
+	}
+
+	public void robotMain() {
+		while (true) {
+			while (isEnabled()) {
+				while (isAutonomous()) {
+					System.out.println("Auto Running!");
+				}
+				while (isOperatorControl()) {
+					setMotors(controller.getRawAxis(STICK_LEFT_VERT), controller.getRawAxis(STICK_RIGHT_VERT));
+				}
+				if (!isAutonomous() && !isOperatorControl())
+					System.err.println("Enabled, but NOT Auto or Telep! Baka!");
+			}
+			while (isDisabled()) {
+				left_1.set(0);
+				left_2.set(0);
+				left_3.set(0);
+				right_1.set(0);
+				right_2.set(0);
+				right_3.set(0);
+
+				intakarino.set(0);
+				cocker.set(0);
+				extendo.set(0);
+				that_thing_that_hangs_1.set(0);
+				that_thing_that_hangs_2.set(0);
+				defensomatic.set(0);
+			}
+			if (!isEnabled() && !isDisabled())
+				System.err.println("Not Enabled or Disabled? Coma? FUCK! What happened?");
+		}
+	}
+
+	private void setMotors(double left, double right) {
+		left_1.set(left);
+		left_2.set(left);
+		left_3.set(left);
+		right_1.set(-right);
+		right_2.set(-right);
+		right_3.set(-right);
+	}
+
+	/*
+	 * String autoSelected = (String) chooser.getSelected();
+	 * 
+	 * SendableChooser chooser;
+	 * 
+	 * final String defaultAuto = "Default"; final String auto1 = "My Auto 1";
+	 * final String auto2 = "My Auto 2"; final String auto3 = "My Auto 3"; final
+	 * String auto4 = "My Auto 4"; final String auto5 = "My Auto 5"; final
+	 * String auto6 = "My Auto 6";
+	 * 
+	 * chooser = new SendableChooser(); chooser.addDefault("Default Auto",
+	 * defaultAuto); chooser.addObject("My Auto 1", auto1); chooser.addObject(
+	 * "My Auto 2", auto2); chooser.addObject("My Auto 3", auto3);
+	 * chooser.addObject("My Auto 4", auto4); chooser.addObject("My Auto 5",
+	 * auto5); chooser.addObject("My Auto 6", auto6); SmartDashboard.putData(
+	 * "Auto modes", chooser);
 	 */
-    public void autonomous() {
-    	
-    	String autoSelected = (String) chooser.getSelected();
-//		String autoSelected = SmartDashboard.getString("Auto Selector", defaultAuto);
-		System.out.println("Auto selected: " + autoSelected);
-    	
-    	switch(autoSelected) {
-    	case customAuto:
-            myRobot.setSafetyEnabled(false);
-            myRobot.drive(-0.5, 1.0);	// spin at half speed
-            Timer.delay(2.0);		//    for 2 seconds
-            myRobot.drive(0.0, 0.0);	// stop robot
-            break;
-    	case defaultAuto:
-    	default:
-            myRobot.setSafetyEnabled(false);
-            myRobot.drive(-0.5, 0.0);	// drive forwards half speed
-            Timer.delay(2.0);		//    for 2 seconds
-            myRobot.drive(0.0, 0.0);	// stop robot
-            break;
-    	}
-    }
-
-    /**
-     * Runs the motors with arcade steering.
-     */
-    public void operatorControl() {
-        myRobot.setSafetyEnabled(true);
-        while (isOperatorControl() && isEnabled()) {
-            myRobot.arcadeDrive(stick); // drive with arcade style (use right stick)
-            Timer.delay(0.005);		// wait for a motor update time
-        }
-    }
-
-    /**
-     * Runs during test mode
-     */
-    public void test() {
-    }
 }
